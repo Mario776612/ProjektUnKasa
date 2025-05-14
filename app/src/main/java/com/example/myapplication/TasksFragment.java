90814ffd35d71de29e4fec92750951829f7d7a93
robertkar <kareckirobert@gmail.com>
2025-05-15 01:47:13 +0200

    Dodatkowe menu i ogolnie jakies zmiany

---
+++ app/src/main/java/com/example/myapplication/TasksFragment.java
@@ -0,0 +1,22 @@
+package com.example.myapplication;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class TasksFragment extends Fragment {
+    private Context context;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_tasks, container, false);
+        context = getContext();
+
+        return view;
+    }
+}
